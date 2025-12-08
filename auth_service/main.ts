675af9c368074edbee9616606d369a68afd4f6e1
SakharovIvan <i731901@gmail.com>
2025-12-08 22:03:27 +0300

    added auth service

--- auth_service/main.ts
+++ auth_service/main.ts
@@ -1,5 +1,26 @@
-export default interface Valid_User {
+export interface Valid_User {
   id: string;
   email: string;
   user: string;
+}
+export interface UserModel{
+  id: string;
+
+  email: string;
+
+  password: string;
+
+  isActivated: boolean;
+
+  activationLink: string;
+}
+
+export interface Token_Schema{
+    id: string;
+  
+    email: string;
+  
+    user: string;
+  
+    refreshToken: string;
 }
