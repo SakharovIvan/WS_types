516bdbf793bb86ef945cc94234ca6b419dee109e
SakharovIvan <i731901@gmail.com>
2025-12-19 17:15:38 +0300

    auth dto added

---
+++ auth_service/auth.dto.ts
@@ -0,0 +1,21 @@
+export class AuthDto{
+  readonly email: string;
+  readonly password: string;
+}
+
+export class MainRegistrationUserDto extends AuthDto{
+  readonly name: string;
+}
+
+export class CreateUserDto extends MainRegistrationUserDto {
+  readonly isActivated: boolean;
+  readonly activationLink: string;
+}
+
+export class UpdateUserDto {
+  readonly name: string | null;
+  readonly email: string | null;
+  readonly password: string | null;
+  readonly isActivated: boolean | null;
+  readonly activationLink: string | null;
+}
--- auth_service/main.ts
+++ auth_service/main.ts
@@ -3,33 +3,45 @@ export interface Valid_User {
   email: string;
   user: string;
 }
-export interface UserModel{
+export interface UserModel {
   id: string;
-
   email: string;
-
   password: string;
-
   isActivated: boolean;
-
   activationLink: string;
 }
 
-export interface Token_Schema{
-    id: string;
-  
-    email: string;
-  
-    user: string;
-  
-    refreshToken: string;
+export interface Token_Schema {
+  id: string;
+  email: string;
+  user: string;
+  refreshToken: string;
 }
 export enum AUTH_ROUTES {
   MAIN = '/api/auth',
-  REGISTRATION = '/registration',
-  LOGIN = '/login',
-  LOGOUT = '/logout',
-  ACTIVATION_LINK = '/activate',
+  SIGN_UP = '/registration',
+  SIGN_IN = '/login',
+  SIGN_OUT = '/logout',
   REFRESH_TOKEN = '/refresh',
-  GET_USERS = '/users',
+  ACTIVATION_LINK = '/activate',
+  PASSWORD_RESET = '/resetpassword',
+  USERS = '/users',
+}
+
+export enum AUTH_CMD {
+  registration = 'registration',
+  login = 'login',
+  logout = 'logout',
+  refresh = 'refresh',
+  activate = 'activate',
+  resetpassword = 'resetpassword',
+  users = 'users',
+}
+export interface JWTPayload {
+  email: string;
+  sub: string;
+}
+export interface AuthTokens {
+  refreshToken: string;
+  accessToken: string;
 }
