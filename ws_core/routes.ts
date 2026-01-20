cf08fca88143dffa5657dbf0d770a3c8023bbc83
SakharovIvan <i731901@gmail.com>
2026-01-20 15:58:12 +0300

    added repair routes

--- ws_core/routes.ts
+++ ws_core/routes.ts
@@ -1,10 +1,4 @@
 export enum REPAIR_ROUTES {
-  MAIN = '/api/auth',
-  SIGN_UP = '/registration',
-  SIGN_IN = '/login',
-  SIGN_OUT = '/logout',
-  REFRESH_TOKEN = '/refresh',
-  ACTIVATION_LINK = '/activate',
-  PASSWORD_RESET = '/resetpassword',
-  USERS = '/users',
+  MAIN = '/api/repair',
+  lsit = '/list',
 }
