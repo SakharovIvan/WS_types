a199407d889492a7343430345156e53511716d90
SakharovIvan <i731901@gmail.com>
2026-02-25 15:25:37 +0300

    catalogue roputes added

--- catalogue/CMDs.ts
+++ catalogue/CMDs.ts
@@ -1,3 +1,7 @@
+import { Mat_No_Analog, Material_No } from './Mat_No';
+import { Product_Mat_No } from './Product';
+import { Work } from './Work';
+
 export enum CATALOGUE {
   MATERIAL,
   ANALOGUE,
@@ -16,6 +20,34 @@ export enum CRUD {
 }
 
 export interface CATALOGUE_CMD {
-  catalogue: CATALOGUE;
+  catalogue: CATALOGUE | string;
   crud: CRUD;
+}
+
+export interface material_commands {
+  material_full_update(data: Partial<Material_No>[]): Promise<void>;
+  material_upsert(data: Partial<Material_No>[]): Promise<void>;
+  get_material(data: Partial<Material_No>): Promise<Material_No>;
+  search_material(daata: string): Promise<Material_No>;
+
+  get_analogue(data: Partial<Material_No>): Promise<Mat_No_Analog>;
+  analogue_full_update(data: Partial<Mat_No_Analog>): Promise<void>;
+  analogue_upsert(data: Partial<Mat_No_Analog>): Promise<void>;
+}
+export interface product_commands {
+  update_mat_no(data: Omit<Product_Mat_No, 'id'>): Promise<void>;
+  get_products;
+}
+export interface work_commands {
+  full_update(data: Partial<Work>[]): Promise<void>;
+  search(data: string): Promise<Work[]>;
+  get(data: Partial<Work>): Promise<Work>;
+  part_update(data: Partial<Work>): Promise<void>;
+}
+
+export interface Catalogue_ClientProxy {
+  send(pattern: CATALOGUE_CMD, simulation: any): any;
+  connect(): any;
+  close(): any;
+  routingMap(): any;
 }
---
+++ catalogue/routes.ts
@@ -0,0 +1,10 @@
+export enum CATALOGUE_ROUTES {
+  MAIN = '/api/catalogue',
+  SEARCH = '/search',
+  MATERIAL = '/material',
+  ANALOGUE = '/analogue',
+  PRODUCT = '/product',
+  PRODUCT_TYPE = '/product_type',
+  PRODUCT_MAT_NO = '/product_mat_no',
+  SCHEME = '/scheme',
+}
