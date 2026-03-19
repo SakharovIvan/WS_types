46970cf18c551aaf17e6a655151f39a19a2af981
SakharovIvan <i731901@gmail.com>
2026-03-19 19:35:02 +0300

    file storage added

---
+++ file_storage/interface.ts
@@ -0,0 +1,21 @@
+import { Bucket, BufferedFile, File } from './types';
+
+export interface Bucket_func {
+  create_bucket(data: Bucket): Promise<void>;
+  get_bucket(id: string): Promise<Bucket>;
+  delete_bucket(id: string): Promise<void>;
+  get_buckets(data: Partial<Bucket>): Promise<Bucket[]>;
+  get_bucket_files(name: string): Promise<File[]>;
+  upload_file(file: BufferedFile, bucket_id: string): Promise<void>;
+  delete_file(data: { file: File; bucket_id: string }): Promise<void>;
+}
+
+export enum FILE_STORAGE_CMDS {
+  create_bucket,
+  get_bucket,
+  delete_bucket,
+  get_buckets,
+  get_bucket_files,
+  upload_file,
+  delete_file,
+}
---
+++ file_storage/types.ts
@@ -0,0 +1,49 @@
+export type Bucket = {
+  id: string;
+
+  name: string;
+  description: string | description;
+  isPublic: boolean;
+  isProtected: boolean;
+  isPrivate: boolean;
+  files: File[];
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type description = 'product' | 'repair' | 'chat';
+
+export type File = {
+  id: string;
+  url: string;
+  name: string;
+  bucket: Bucket;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export interface BufferedFile {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: AppMimeType;
+  size: number;
+  buffer: Buffer | string;
+}
+
+export interface StoredFile extends HasFile, StoredFileMetadata {}
+
+export interface HasFile {
+  file: Buffer | string;
+}
+export interface StoredFileMetadata {
+  id: string;
+  name: string;
+  encoding: string;
+  mimetype: AppMimeType;
+  size: number;
+  updatedAt: Date;
+  fileSrc?: string;
+}
+
+export type AppMimeType = 'image/png' | 'image/jpeg';
