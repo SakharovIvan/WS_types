40539334fa3567862f89156cb8f2c476ae079a2c
SakharovIvan <i731901@gmail.com>
2026-05-13 17:09:35 +0300

    notification added

---
+++ notification/routes.ts
@@ -0,0 +1,7 @@
+export enum Notification_types_CMDs {
+  registration,
+  refresh_password,
+  success_refresh_password,
+  closePeriod,
+  new_messages,
+}
---
+++ notification/types.ts
@@ -0,0 +1,5 @@
+export interface Email {
+  id: string;
+  text: string;
+  type: string;
+}
